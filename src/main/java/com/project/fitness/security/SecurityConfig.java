265fcd7d4fb5796ea97e342fb3a7e0d866559b0c
Aayush Maheshwari <aayushmaheshwarigi111@gmail.com>
2026-05-01 23:35:18 +0530

    Add: Spring Security Authentication via InMemory UserDetails (#4)
    * Brnach protection on public repo
    
    * Add: Spring Security Authentication via InMemory UserDetails
    
    ---------
    
    Co-authored-by: Aayush Maheshwari <AayushMaheshwari@users.noreply.github.com>

--- src/main/java/com/project/fitness/security/SecurityConfig.java
+++ src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +21,18 @@ public class SecurityConfig {
         );
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+        .password("{noop}password1")
+        .build();
+
+        UserDetails user2 = User.withUsername("user2")
+        .password("{noop}password2")
+        .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }

e85c3eb87722f09b3947a95a05f9b1190a36f519
Aayush Maheshwari <AayushMaheshwari@users.noreply.github.com>
2026-05-06 22:31:40 +0530

    Feature: Added Method Level Security for Activity Controller.java

--- src/main/java/com/project/fitness/controller/ActivityController.java
+++ src/main/java/com/project/fitness/controller/ActivityController.java
@@ -3,6 +3,7 @@ package com.project.fitness.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,11 +24,13 @@ public class ActivityController {
 
     private final ActivityService activityService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<ActivityResponseDTO> createActivity(@RequestBody ActivityRequestDTO activityRequestDTO) {
         return ResponseEntity.ok(activityService.createActivity(activityRequestDTO));
     }
 
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/{userId}")
     public ResponseEntity<List<ActivityResponseDTO>> getUserActivities(@PathVariable String userId) {
         return ResponseEntity.ok(activityService.getUserActivities(userId));

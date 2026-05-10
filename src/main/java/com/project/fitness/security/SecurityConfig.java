3467e3898b08ffb89785367f164f47e3de18b921
Aayush Maheshwari <aayushmaheshwarigi111@gmail.com>
2026-05-10 23:53:23 +0530

    Feature: Add JwtToken Authentication for Login (#11)
    * Brnach protection on public repo
    
    * Feature: Add JwtToken Authentication for Login
    
    ---------
    
    Co-authored-by: Aayush Maheshwari <AayushMaheshwari@users.noreply.github.com>

--- src/main/java/com/project/fitness/security/SecurityConfig.java
+++ src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -5,7 +5,9 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +28,9 @@ public class SecurityConfig {
 
     @Autowired
     private DataSource datasource;
+
+    @Autowired
+    private AuthTokenFilter authTokenFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -32,9 +38,11 @@ public class SecurityConfig {
                         authorizeRequests -> authorizeRequests
                                 .requestMatchers("/api/recommendations/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/api/recommendations/**").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                );
+        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                // .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
@@ -64,5 +72,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
